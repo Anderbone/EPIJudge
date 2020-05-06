5f3c81bb32d4df732ed4193c3c5e6aa7617be6ec
Anderbone <yanchiyu@outlook.com>
2020-05-06 22:53:37 +0100

    epi 5.1

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,30 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class Parity {
-  @EpiTest(testDataFile = "parity.tsv")
-  public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class Parity {
+    @EpiTest(testDataFile = "parity.tsv")
+    public static short parity(long x) {
+        short flag = 0;
+        while (x > 0) {
+            short last = (short) (x & 1);
+            flag = (short) (flag ^ last);
+            x = x >> 1;
+
+
+//            flag ^= 1;
+//            x = x&(x-1);
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "Parity.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
