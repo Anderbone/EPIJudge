070f5616aaf0496e9e3d8c64dfd92b3d845f7924
Anderbone <yanchiyu@outlook.com>
2020-05-15 21:34:12 +0100

    5.3， 5.4

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -1,18 +1,32 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ClosestIntSameWeight {
-  @EpiTest(testDataFile = "closest_int_same_weight.tsv")
-  public static long closestIntSameBitCount(long x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ClosestIntSameWeight.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class ClosestIntSameWeight {
+    @EpiTest(testDataFile = "closest_int_same_weight.tsv")
+    public static long closestIntSameBitCount(long x) {
+
+        for (int i = 0; i < 64; i++) {
+            short end1 = (short) (x & (1<<i));
+            short end2 = (short) (x & (1<<(i+1)));
+            if ((end1 != end2)&&(end1 == 0 || end2 == 0)) {
+                // reverse these last two numbers
+                x = (1<<i) ^ x;
+                x = (1<<(i+1)) ^ x;
+                return x;
+            }
+        }
+        return x;
+
+}
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ClosestIntSameWeight.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -1,18 +1,29 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class ReverseBits {
-  @EpiTest(testDataFile = "reverse_bits.tsv")
-  public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "ReverseBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class ReverseBits {
+    @EpiTest(testDataFile = "reverse_bits.tsv")
+    public static long reverseBits(long x) {
+        long result = 0;
+        int count = 0;
+        while (x > 0) {
+            count++;
+            short rightBit = (short) (x & 1);
+            result = (result << 1) + rightBit;
+            x >>= 1;
+        }
+        result <<= (64 - count);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "ReverseBits.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
