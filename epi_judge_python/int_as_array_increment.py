03355644f74ead13cbf117b6caef1a09369a5d0a
Anderbone <yanchiyu@outlook.com>
2020-11-26 21:01:03 +0000

    plus one

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -5,7 +5,23 @@ from test_framework import generic_test
 
 def plus_one(A: List[int]) -> List[int]:
     # TODO - you fill in here.
-    return []
+
+    # 1,2,9  -> 1,3,0
+    # if last is 9, change to 0, add 1 to the next.
+    # if first is 9, change to 0, add 1 in the first.
+    count0_right = 0
+    # for i, v in enumerate(A[::-1]):
+    for i, v in enumerate(reversed(A)):
+        index = len(A) - 1 - i
+        if v != 9:
+            v += 1
+            res = A[:index] + [v] + [0] * count0_right
+            break
+        count0_right += 1
+    if count0_right == len(A):
+        return [1] + [0] * len(A)
+        return A
+    return res
 
 
 if __name__ == '__main__':
