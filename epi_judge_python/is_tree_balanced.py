ed9a6300bb7325e8f37dd4025079d6c224966641
Anderbone <yanchiyu@outlook.com>
2020-12-06 15:38:08 +0000

    tree start2

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,8 +1,10 @@
+import collections
+
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-# def is_balanced_binary_tree(tree):
+# def is_balanced_binary_tree(tree):   # old
 #     def height(root):
 #         if not root:
 #             return 0
@@ -36,7 +38,26 @@ from test_framework import generic_test
 #         tree = node.right
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+    NodeStatus = collections.namedtuple('NodeStatus',('isBalanced','height'))
+    def check_balanced(tree):
+        if not tree:
+            return NodeStatus(True, -1)
+
+        leftStatus = check_balanced(tree.left)
+        if not leftStatus.isBalanced:
+            return NodeStatus(False, 0)
+
+        rightStatus = check_balanced(tree.right)
+        if not rightStatus.isBalanced:
+            return NodeStatus(False, 0)
+
+        isBalanced = abs(leftStatus.height - rightStatus.height) <= 1
+        height = 1 + max(leftStatus.height, rightStatus.height)
+        return NodeStatus(isBalanced, height)
+
+    return check_balanced(tree).isBalanced
+
+def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
     def height(root):
         if not root:
             return 0
@@ -50,17 +71,9 @@ def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
         height = 0
         level = [root]
         while level:
-            # if not level:
-            #     return 0
             height += 1
             level = [i for n in level for i in (n.left, n.right) if i]
         return height
-
-    # post order? left, right, root. once not balanced found, return false
-    def post_order(root):
-        post_order(root.left)
-        post_order(root.right)
-        print(root)
 
     level = [tree]
     if not level:
@@ -70,20 +83,9 @@ def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
             if not node:
                 return True
             if abs(height2(node.left) - height2(node.right)) > 1:
+            # if abs(height(node.left) - height(node.right)) > 1:
                 return False
         level = [i for n in level for i in (n.left, n.right) if i]
-
-    # stack = []
-    # while True:
-    #     while tree:
-    #         stack.append(tree)
-    #         tree = tree.left
-    #     if not stack:
-    #         return True
-    #     node = stack.pop()
-    #     if abs(height(node.left) - height(node.right)) > 1:
-    #         return False
-    #     tree = node.right
 
     return True
 
