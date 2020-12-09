c6e488ea5ec3c1c71ad71939194cd06e3b3e6b9e
Anderbone <yanchiyu@outlook.com>
2020-12-09 12:05:39 +0000

    hash table

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,19 +1,29 @@
 from test_framework import generic_test
 import collections
 
-<<<<<<< HEAD
-def can_form_palindrome(s):
-    sum = 0
-    for i in collections.Counter(s).values():
-        sum += i%2
-    if sum <=1:
-        return True
-    return False
-=======
+
+# def can_form_palindrome(s):
+#     sum = 0
+#     for i in collections.Counter(s).values():
+#         sum += i%2
+#     if sum <=1:
+#         return True
+#     return False
 def can_form_palindrome(s: str) -> bool:
     # TODO - you fill in here.
+    # counter = collections.defaultdict(lambda: 0)
+    # s = "".join(s.split())
+    # for i in s:
+    #     counter[i] += 1
+
+    counter = collections.Counter(s)
+    single = 0
+    for v in counter.values():
+        if v % 2 == 1:
+            single += 1
+        if single > 1:
+            return False
     return True
->>>>>>> upstream/master
 
 
 if __name__ == '__main__':
