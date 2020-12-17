f2b20add1d953dee54e17ce3c08f1c2fccf74246
Anderbone <yanchiyu@outlook.com>
2020-12-17 16:40:13 +0000

    random.sample

--- epi_judge_python_solutions/offline_sampling.py
+++ epi_judge_python_solutions/offline_sampling.py
@@ -11,10 +11,12 @@ from test_framework.test_utils import enable_executor_hook
 
 def random_sampling(k: int, A: List[int]) -> None:
 
-    for i in range(k):
-        # Generate a random index in [i, len(A) - 1].
-        r = random.randint(i, len(A) - 1)
-        A[i], A[r] = A[r], A[i]
+    # for i in range(k):
+    #     # Generate a random index in [i, len(A) - 1].
+    #     r = random.randint(i, len(A) - 1)
+    #     A[i], A[r] = A[r], A[i]
+    A[:] = random.sample(A, k)
+
 
 
 # Pythonic solution
